dda06ffa894b7a532651cbf18104b5102e1a61a3
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-11-03 07:03:34 +0100

    modif avis

--- app/Models/Avis.php
+++ app/Models/Avis.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Model;
 class Avis extends Model
 {
     use HasFactory;
-    
+
     protected $fillable = [
         'auteur_id',
         'validateur_id',
@@ -24,17 +24,17 @@ class Avis extends Model
     {
         return $this->belongsTo(User::class, 'auteur_id');
     }
-    
+
     public function validateur()
     {
         return $this->belongsTo(User::class, 'validateur_id');
     }
-    
+
     public function concerne()
     {
         return $this->belongsTo(User::class, 'concerne_id');
     }
-    
+
     public function covoiturage()
     {
         return $this->belongsTo(Covoiturage::class, 'covoiturage_id');
