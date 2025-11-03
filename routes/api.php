37af6194791d59caf840a01e88697b2db12681f2
Nade04 <86410129+Nade0478@users.noreply.github.com>
2025-11-03 07:11:13 +0100

    modif routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,29 @@
 <?php
 
+use App\Http\Controllers\API\AdministrateurController;
+use App\Http\Controllers\API\AvisController;
+use App\Http\Controllers\API\ConfigurationController;
+use App\Http\Controllers\API\CovoiturageController;
+use App\Http\Controllers\API\EmployeController;
+use App\Http\Controllers\API\ParticipationController;
+use App\Http\Controllers\API\PreferenceController;
+use App\Http\Controllers\API\UserController;
+use App\Http\Controllers\API\VehiculeController;
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\UserController;
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
     });
-    Route::apiResource('users', UserController::class);
+    Route::apiResource('Users', UserController::class);
+    Route::apiResource('Covoiturages', CovoiturageController::class);
+    Route::apiResource('Participations', ParticipationController::class);
+    Route::apiResource('Configurations', ConfigurationController::class);
+    Route::apiResource('Employes', EmployeController::class);
+    Route::apiResource('Administrateurs', AdministrateurController::class);
+    Route::apiResource('Vehicules', VehiculeController::class);
+    Route::apiResource('Preferences', PreferenceController::class);
+    Route::apiResource('Avis', AvisController::class);
+
